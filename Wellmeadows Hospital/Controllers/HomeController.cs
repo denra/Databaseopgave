6e249fd18a5d4895fdfe688750e5d8eb90f0597c
CamillaME <info@camillame.dk>
2018-04-21 11:54:44 +0200

    Update, Get og Delete Supplier
    Har også lige sat dem ind i Home controlleren

--- Wellmeadows Hospital/Controllers/HomeController.cs
+++ Wellmeadows Hospital/Controllers/HomeController.cs
@@ -23,7 +23,9 @@ namespace Wellmeadows_Hospital.Controllers
             staff.GetStaffView();
 
             Supplier.CreateSupplier();
-
+            Supplier.UpdateSupplier();
+            Supplier.GetSupplier();
+            //Supplier.DeleteSupplier();
 
             return View();
         }
